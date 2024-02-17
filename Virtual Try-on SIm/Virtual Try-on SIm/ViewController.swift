75d7c539318d7510dd2e710fb55701649c2903f0
Kayla Son <ksonkayla@gmail.com>
2024-02-17 14:53:04 -0800

    project set up

---
+++ Virtual Try-on SIm/Virtual Try-on SIm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Virtual Try-on SIm
+//
+//  Created by Kayla Son on 2/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
